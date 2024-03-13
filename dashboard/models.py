eaff80b7f0ea47a568b44e1f1ef91a41865b5921
Zulf1qor <zsharibjonov@gmail.com>
2024-03-13 22:00:30 +0500

    zulfiqor

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=0)
+    image = models.ManyToMany(to='Image', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='product_images')
+
+
